ebcb1db3388c84fd2c1937d464caf0c71ba67e43
Jeff Deville <jeffdeville@gmail.com>
2013-01-11 14:54:41 -0500

    handle commandline output from heroku

--- lib/figaro/tasks.rb
+++ lib/figaro/tasks.rb
@@ -6,6 +6,11 @@ module Figaro
       with_app = app ? " --app #{app}" : ""
 
       rails_env = Open3.capture2("heroku config:get RAILS_ENV#{with_app}")
+      rails_env = case rails_env
+        when Array then rails_env.first.chomp
+        else
+          rails_env
+      end
       vars = Figaro.vars(rails_env.presence)
 
       Open3.capture2("heroku config:add #{vars}#{with_app}")
